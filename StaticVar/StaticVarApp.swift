f2e7bc0fef2057e8e975b192c27afcff613277ce
Ruxo <113531412+turangarusso@users.noreply.github.com>
2022-12-06 15:09:34 +0100

    First

---
+++ StaticVar/StaticVarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticVarApp.swift
+//  StaticVar
+//
+//  Created by giovanni russo on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StaticVarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
