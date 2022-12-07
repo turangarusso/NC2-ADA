0958c6e23537259d592ed01b588ee7546101f7dd
Ruxo <113531412+turangarusso@users.noreply.github.com>
2022-12-07 16:58:12 +0100

    Map Model v2

--- NuovaMappa/ContentView.swift
+++ NuovaMappa/ContentView.swift
@@ -15,21 +15,368 @@ enum MapDetails {
     
     static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
 }
-//    .font(.system(size:60))
 
 
+let MapLocations = [
+    MapLocation(name: "Drinking Fountain", latitude: 40.837208, longitude: 14.306232, fontana: true, point: false, recycle: false, address: "Corso N. Protopisani, 70, 80146 Napoli NA", description: "In this location you can drink potable water for free.", imageName: "fountain" ),
+    MapLocation(name: "Waste Depot", latitude: 40.89356260547285, longitude: 14.227367128290185, fontana: false, point: false, recycle: true, address: "Via Campano, 80145 Napoli NA", description: "Waste Recycling Depots are a convenient way to throw away the stuff you don't need anymore.", imageName: "recycle"),
+    MapLocation(name: "Drinking Fountain", latitude: 40.826329, longitude: 14.205032, fontana: true, point: false, recycle: false, address: "Piazza Italia, 80125 Napoli NA", description: "In this location you can drink potable water for free.", imageName: "fountain" ),
+    MapLocation(name: "Drinking Fountain", latitude: 40.8321628, longitude: 14.2407789, fontana: true, point: false, recycle: false, address: "Villa Comunale, 80122 Napoli NA", description: "In this location you can drink potable water for free.", imageName: "fountain" ),
+]
+
+let MapLocationsClass = [
+    MaplocClass(name: "Drinking Fountain", latitude: 40.837208, longitude: 14.306232, fontana: true, point: false, recycle: false, address: "Corso N. Protopisani, 70, 80146 Napoli NA", description: "In this location you can drink potable water for free.", imageName: "fountain" ),
+    MaplocClass(name: "Waste Depot", latitude: 40.89356260547285, longitude: 14.227367128290185, fontana: false, point: false, recycle: true, address: "Via Campano, 80145 Napoli NA", description: "Waste Recycling Depots are a convenient way to throw away the stuff you don't need anymore.", imageName: "recycle"),
+    MaplocClass(name: "Drinking Fountain", latitude: 40.826329, longitude: 14.205032, fontana: true, point: false, recycle: false, address: "Piazza Italia, 80125 Napoli NA", description: "In this location you can drink potable water for free.", imageName: "fountain" ),
+    MaplocClass(name: "Drinking Fountain", latitude: 40.8321628, longitude: 14.2407789, fontana: true, point: false, recycle: false, address: "Villa Comunale, 80122 Napoli NA", description: "In this location you can drink potable water for free.", imageName: "fountain" ),
+]
 
 
 struct ContentView: View {
     
     
+    private enum Field: Int, CaseIterable{
+        case search
+    }
+    
     @StateObject private var MapModel = Map01Model() //location service check
     
+    @State private var text = "" // search bar
+    
+    @State private var pincord = CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868)
+    
+    @State var tog = true
+    
+    @State var isSearching = false // search bar tap check
+    
+    @State private var selectedPlace: MaplocClass? // used for annotations sheet
+    
+    @State var latitude01 = 40.837208
+    
+    @State var longitude01 = 14.306232
+    
+    @FocusState private var focusField: Field?
+    
     var body: some View {
-        Map(coordinateRegion: $MapModel.region2)
+        ZStack{
+            Map(coordinateRegion: $MapModel.region2, showsUserLocation: true,
+                annotationItems: searchResults,
+                annotationContent: { location in
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                    
+                    
+                    VStack {
+                        
+                        if location.fontana {
+                            
+                            Image(systemName: "drop.circle.fill")
+                                .resizable()
+                                .foregroundColor(.green)
+                                .frame(width: 30, height: 30)
+                                .background(.white)
+                                .clipShape(Circle())
+                                .onTapGesture {
+                                    selectedPlace = location
+                                }
+                            
+                                .sheet(item: $selectedPlace) { location in
+                                    VStack{
+                                        
+                                        HStack {
+                                            Text(location.name)
+                                                .font(.system(size:30))
+                                                .padding()
+                                                .foregroundColor(.green)
+                                        }
+                                        Spacer()
+                                        
+                                        Image(location.imageName)
+                                            .resizable()
+                                            .frame(width: 290, height: 290)
+                                            .clipShape(RoundedRectangle(cornerRadius: 6))
+                                            .shadow(color: .green, radius: 10)
+                                            .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.white, lineWidth: 5))
+                                            .foregroundColor(.yellow)
+                                        
+                                        Spacer()
+                                        Text("Address: ")+Text(location.address)
+                                        Spacer()
+                                        Group {
+                                            
+                                            Text(location.description)
+                                            
+                                        } //group end
+                                        .frame(width: 270)
+                                        .padding()
+                                        
+                                        .foregroundColor(.white)
+                                        .overlay(RoundedRectangle(cornerRadius: 6)
+                                            .stroke(Color.green, lineWidth: 3))
+                                        
+                                        Spacer()
+                                        Spacer()
+                                        
+                                        
+                                    }
+                                }
+                            
+                        } else if location.recycle{
+                            
+                            Image(systemName: "leaf.circle.fill")
+                                .resizable()
+                                .foregroundColor(.green)
+                                .frame(width: 30, height: 30)
+                                .background(.white)
+                                .clipShape(Circle())
+                                .onTapGesture {
+                                    selectedPlace = location
+                                }
+                            
+                                .sheet(item: $selectedPlace) { location in
+                                    VStack{
+                                        
+                                        HStack {
+                                            Text(location.name)
+                                                .font(.system(size:30))
+                                                .padding()
+                                                .foregroundColor(.green)
+                                        }
+                                        Spacer()
+                                        
+                                        Image(location.imageName)
+                                            .resizable()
+                                            .frame(width: 290, height: 290)
+                                            .clipShape(RoundedRectangle(cornerRadius: 6))
+                                            .shadow(color: .green, radius: 10)
+                                            .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.white, lineWidth: 5))
+                                            .foregroundColor(.yellow)
+                                        
+                                        Spacer()
+                                        Text("Address: ")+Text(location.address)
+                                        Spacer()
+                                        Group {
+                                            
+                                            Text(location.description)
+                                            
+                                        } //group end
+                                        .frame(width: 270)
+                                        .padding()
+                                        
+                                        .foregroundColor(.white)
+                                        .overlay(RoundedRectangle(cornerRadius: 6)
+                                            .stroke(Color.green, lineWidth: 3))
+                                        
+                                        Spacer()
+                                        Spacer()
+                                        
+                                        
+                                    }// end VStack
+                                }
+                            
+                        } else {
+                            
+                            Image(systemName: "mappin.circle")
+                                .resizable()
+                                .foregroundColor(.green)
+                                .frame(width: 30, height: 30)
+                                .background(.white)
+                                .clipShape(Circle())
+                                .onTapGesture {
+                                    selectedPlace = location
+                                }
+                            
+                                .sheet(item: $selectedPlace) { location in
+                                    VStack{
+                                        
+                                        HStack {
+                                            Text(location.name)
+                                                .font(.system(size:30))
+                                                .padding()
+                                                .foregroundColor(.green)
+                                        }
+                                        Spacer()
+                                        
+                                        Image(location.imageName)
+                                            .resizable()
+                                            .frame(width: 290, height: 290)
+                                            .clipShape(RoundedRectangle(cornerRadius: 6))
+                                            .shadow(color: .green, radius: 10)
+                                            .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.white, lineWidth: 5))
+                                            .foregroundColor(.yellow)
+                                        
+                                        Spacer()
+                                        Text("Address: ")+Text(location.address)
+                                        Spacer()
+                                        Group {
+                                            
+                                            Text(location.description)
+                                            
+                                        } //group end
+                                        .frame(width: 270)
+                                        .padding()
+                                        
+                                        .foregroundColor(.white)
+                                        .overlay(RoundedRectangle(cornerRadius: 6)
+                                            .stroke(Color.green, lineWidth: 3))
+                                        
+                                        Spacer()
+                                        Spacer()
+                                        
+                                        
+                                    }
+                                }
+                        }
+                        
+                        
+                        Text(location.name)
+                            .frame(width: 70, height: 50)
+                        
+                        
+                    }
+                }
+            })
             .ignoresSafeArea()
-
+            .preferredColorScheme(.dark)
+            .onAppear{
+                MapModel.checkLocation()
+            }// check on appear if location service is active
+            .alert(isPresented: $MapModel.pd, content: {
+                
+                Alert(title: Text("Permission denied"), message: Text("Please Enable Permission In App Settings"), dismissButton: .default(Text("Go to Settings"), action: {
+                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                    
+                }))
+            }) //end Alert popup for locations settings
+            
+            
+            
+            //            .padding()
+            VStack(alignment: .center){
+                
+                
+                HStack{
+                    TextField(" What are you looking for?", text: $text) //search bar
+                        .padding(.leading, 24.0)
+                        .padding(.horizontal, 24.0)
+                    
+                }
+                
+                .padding()
+                .background()
+                .cornerRadius(6)
+                .padding(.horizontal)
+                .onTapGesture(perform: {
+                    if !isSearching{
+                        isSearching.toggle()}})
+                .onSubmit{
+                    punta()
+                    MapModel.region2 = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude01, longitude: longitude01), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+                }
+                .submitLabel(.search)
+                .overlay(
+                    HStack{
+                        
+                        Image(systemName: "magnifyingglass")
+                        Spacer()
+                        
+                        if isSearching && text != ""{
+                            Button(action: { text = ""}, label: {Image(systemName: "xmark.circle")})
+                            
+                        }// show delete search text button
+                        
+                    }.padding(.horizontal, 32) )
+                
+                
+                
+                if text != "" && !searchResults.isEmpty {
+                    ScrollView{
+                        
+                        VStack(spacing: 1){
+                            ForEach(searchResults){location in
+                                
+                                Text(location.name)
+                                
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 16, weight: .light))
+                                    .foregroundColor(.black)
+                                    .frame(width: 150, height: 50, alignment: .leading)
+                                    .onTapGesture {
+                                        
+                                        MapModel.region2 = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+                                        text = ""
+                                        
+                                    }
+                                Text(location.address)
+                                    .font(.system(size: 12, weight: .light))
+                                    .foregroundColor(.black)
+                                    .frame(width: 150, height: 50, alignment: .leading)
+                                
+                                Divider()
+                                
+                            }
+                        }.padding()
+                    }.background(Color.white)
+                        .opacity(30)
+                        .cornerRadius(6)
+                    //                        .padding(.bottom, 500.0)
+                    
+                }
+                
+                
+                
+                
+                Spacer()
+            }//end VStack
+            .padding()
+            
+            VStack{
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {focusLocation()}, label: {
+                        Image(systemName: "paperplane.fill")
+                            .font(.title2)
+                            .padding(10)
+                            .background(Color.black)
+                            .clipShape(Circle())
+                            .padding()
+                        
+                    })
+                }
+            }
+            
+        } //end ZStack
+        
     }
+    
+    var prova: [MapLocation]{
+        return MapLocations.filter { $0.fontana == true }
+    } //select only fountains
+    
+    
+    
+    var searchResults: [MaplocClass] {
+        if text.isEmpty {
+            return MapLocationsClass
+        } else {
+            return MapLocationsClass.filter { $0.name.contains(text) }
+        }
+    }//search a point
+    
+    func punta(){
+        if let idx = searchResults.firstIndex(where: { $0.name.contains(text)}) {
+            longitude01 = searchResults[idx].longitude
+            latitude01 = searchResults[idx].latitude
+        }
+    }// center map on the annotation
+    
+    func focusLocation(){
+        MapModel.region2 = MKCoordinateRegion(center: CLLocationManager().location?.coordinate ?? MapDetails.startingLocation, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+        
+    }// center map on the user location if location service is running
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- NuovaMappa/Locations.swift
+++ NuovaMappa/Locations.swift
@@ -35,3 +35,41 @@ struct MapLocation: Identifiable {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
+
+class MaplocClass: Identifiable {
+        
+    let name: String
+    
+    let latitude: Double
+    
+    let longitude: Double
+    
+    let fontana: Bool
+    
+    let point: Bool
+    
+    let recycle: Bool
+    
+    let address: String
+    
+    let description: String
+    
+    let imageName: String
+    
+    init(name: String, latitude: Double, longitude: Double, fontana: Bool, point: Bool, recycle: Bool, address: String, description: String, imageName: String){
+        
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.fontana = fontana
+        self.point = point
+        self.recycle = recycle
+        self.address = address
+        self.description = description
+        self.imageName = imageName
+        
+    }
+        
+    
+}
+
